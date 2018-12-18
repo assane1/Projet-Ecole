8adf3f8b4ee17672780bb4c22be3051da33a1f53
sakine assane <sakineassane@gmail.com>
2018-12-18 17:03:38 +0000

    Update admin_dashboard.php
    test

--- application/views/backend/admin/admin_dashboard.php
+++ application/views/backend/admin/admin_dashboard.php
@@ -11,7 +11,7 @@ $system_name        =   $this->db->get_where('settings' , array('type'=>'system_
     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
         <ol class="breadcrumb">
             <li><a href="<?php echo base_url(); ?>index.php?admin/admin_dashboard"><?php echo $system_title; ?></a></li>
-            <li class="active"><?php echo get_phrase('Admin-Dashboardhh');?></li>
+           
         </ol>
     </div>
 
