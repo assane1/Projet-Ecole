49e3bac3c8d95d181ce76b9aa74ed0273b2ebfda
mbodjibara <45972000+mbodjibara@users.noreply.github.com>
2018-12-18 17:27:55 +0000

    Update add_student.php
    add bara

--- application/views/backend/admin/add_student.php
+++ application/views/backend/admin/add_student.php
@@ -4,8 +4,7 @@
     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
         <ol class="breadcrumb">
             <li><a href="<?php echo base_url();?>index.php?admin/admin_dashboard"><?php echo get_phrase('Dashboard'); ?></a></li>
-            <li class="active"><?php echo get_phrase('Add-Student'); ?></li>
-                        <li class="active"><?php echo get_phrase('nouveaux'); ?></li>
+            <li class="active"><?php echo get_phrase('Add-Bara'); ?></li>
         </ol>
     </div>
 </div>
