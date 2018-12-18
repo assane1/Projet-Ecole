0d9d2447085d5db3bae2681cdae58aa86d9a7fa7
guissemohamedcherif <44408461+guissemohamedcherif@users.noreply.github.com>
2018-12-18 18:13:41 +0000

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$config['base_url'] = 'http://localhost/SamaEcole/';
+$config['base_url'] = 'http://localhost/SamaEcole';
 $config['index_page'] = 'index.php';
 $config['uri_protocol']	= 'QUERY_STRING';
 $config['url_suffix'] = '';
