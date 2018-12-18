c0d04c1638d37bb1c5464db8fb093cea4d30c5d0
mbodjibara <45972000+mbodjibara@users.noreply.github.com>
2018-12-18 16:31:43 +0000

    Update admin_dashboard.php
    correction sur les div

--- application/views/backend/admin/admin_dashboard.php
+++ application/views/backend/admin/admin_dashboard.php
@@ -5,10 +5,12 @@ $system_name        =   $this->db->get_where('settings' , array('type'=>'system_
 
 <div class="row bg-title">
     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-        <h4 class="page-title"><?php echo get_phrase('Admin-Dashboard');?></h4> </div>
+        <h4 class="page-title"><?php echo get_phrase('Admin-Dashboard');?></h4> 
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+        <h4 class="page-title"><?php echo get_phrase('bara mbodji etudiant en master');?></h4> 
+    </div>
     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
-        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-        <h4 class="page-title"><?php echo get_phrase('bara mbodji etudiant en master');?></h4> </div>
     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
         <ol class="breadcrumb">
             <li><a href="<?php echo base_url(); ?>index.php?admin/admin_dashboard"><?php echo $system_title; ?></a></li>
